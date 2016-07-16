bd74aaeefeee4740258be63ccc0de048dd081646
Akhil munna <Akhil munna>
2016-07-16 20:20:04 +0530

    initial commit Mastering Extjs

---
+++ app/Application.js
@@ -0,0 +1,17 @@
+Ext.define('Packt.Application', {
+    name: 'Packt',
+
+    extend: 'Ext.app.Application',
+
+    views: [
+        // TODO: add views here
+    ],
+
+    controllers: [
+        // TODO: add controllers here
+    ],
+
+    stores: [
+        // TODO: add stores here
+    ]
+});
