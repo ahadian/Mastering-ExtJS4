028c6fa3bd1293f7c7408899d6724f5c87655397
Akhil munna <Akhil munna>
2016-07-16 22:49:45 +0530

    login form -With user name and password -Added basic client validations

--- app/view/Login.js
+++ app/view/Login.js
@@ -1,16 +1,46 @@
-Ext.define('Packt.view.Login',{
-    extend:'Ext.window.Window',
-    alias:'widget.login',
+Ext.define('Packt.view.Login', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.login',
 
-    autoShow:true,
-    height:170,
-    width:360,
-    layout:{
-        type:'fit'
+    autoShow: true,
+    height: 170,
+    width: 360,
+    layout: {
+        type: 'fit'
     },
-    iconCls:'key',
-    title:'Login',
-    closeAction:'hide',
-    closable:false,
-    resizable:false
+    items: [
+        {
+            xtype: 'form',
+            frame: false,
+            bodyPadding: 15,
+            defaults: {
+                xtype: 'textfield',
+                anchor: '100%',
+                labelWidth: 60,
+                allowBlank: false,
+                vtype: 'alphanum',
+                minLength: 3,
+                msgTarget: 'under',
+
+            },
+            items: [
+                {
+                    name: 'user',
+                    fieldLabel: 'User',
+                    maxLangth: 25
+                },
+                {
+                    inputType: 'password',
+                    name: 'password',
+                    fieldLabel: 'password',
+                    maxLanth: 15
+                }
+            ]
+        }
+    ],
+    iconCls: 'key',
+    title: 'Login',
+    closeAction: 'hide',
+    closable: false,
+    resizable: false
 });
