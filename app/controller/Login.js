5c6060b086cd69cd4c1a266013f1bb618ce42118
Akhil munna <Akhil munna>
2016-07-17 09:22:38 +0530

    Added submit and cancel events to controller

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -5,7 +5,20 @@ Ext.define('Packt.controller.Login',{
 
     init:function(application){
         this.control({
-
+            'login form button#submit':{
+                click:this.onButtonClickSubmit
+            },
+            'login form button#cancel':{
+                click:this.onButtonClickCancel
+            }
         });
+    },
+
+    onButtonClickCancel:function(){
+        console.log('cancel clicked');
+    },
+
+    onButtonClickSubmit:function(){
+        console.log('submit clicked');
     }
 });
