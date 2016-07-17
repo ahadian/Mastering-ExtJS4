f5850b8b788cee67f027cdd421c135f1f6f849aa
Akhil munna <Akhil munna>
2016-07-17 09:44:13 +0530

    submit functionality -Added ajax request, still server side is not implemented

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -1,24 +1,37 @@
-Ext.define('Packt.controller.Login',{
-    extend:'Ext.app.Controller',
+Ext.define('Packt.controller.Login', {
+    extend: 'Ext.app.Controller',
 
-    views:['Login'],
+    views: ['Login'],
 
-    init:function(application){
+    init: function (application) {
         this.control({
-            'login form button#submit':{
-                click:this.onButtonClickSubmit
+            'login form button#submit': {
+                click: this.onButtonClickSubmit
             },
-            'login form button#cancel':{
-                click:this.onButtonClickCancel
+            'login form button#cancel': {
+                click: this.onButtonClickCancel
             }
         });
     },
 
-    onButtonClickCancel:function(button, e,options){
+    onButtonClickCancel: function (button, e, options) {
         button.up('form').getForm().reset();
     },
 
-    onButtonClickSubmit:function(button, e,options){
-        console.log('submit clicked');
+    onButtonClickSubmit: function (button, e, options) {
+        var formPanel = button.up('form'),
+            login = button.up('login'),
+            user = formPanel.down('textfield[name=user]').getValue(),
+            pass = formPanel.down('textfield[name=password]').getValue();
+
+            if(formPanel.getForm().isValid()){
+                Ext.Ajax.request({
+                    url:'php/login.php',
+                    params:{
+                        user:user,
+                        password:pass
+                    }
+                })
+            }
     }
 });
