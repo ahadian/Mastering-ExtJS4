780189e7f85087d07512da6d002c44fc010036c5
Akhil munna <Akhil munna>
2016-07-17 11:23:07 +0530

    Added success and failure handlers

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -27,12 +27,44 @@ Ext.define('Packt.controller.Login', {
             pass = formPanel.down('textfield[name=password]').getValue();
 
         if (formPanel.getForm().isValid()) {
+            Ext.get(login.getEl()).mask('Authenticating.. Please wait..','Loading');
             Ext.Ajax.request({
                 url: 'php/login.php',
                 params: {
                     user: user,
                     password: Packt.util.MD5.encode(pass)
+                },
+                failure: function (conn, response, options, eOpts) {
+                    Ext.get(login.getEl()).unmask();
+                    Ext.Msg.show({
+                        title: 'Error!',
+                        msg: conn.responseText,
+                        icon: Ext.Msg.ERROR,
+                        buttons: Ext.Msg.OK
+                    });
+                },
+                success: function (conn, response, options, eOpts) {
+                    Ext.get(login.getEl()).unmask();
+                    var result = Ext.JSON.decode(conn.responseText, true); // #1
+                    if (!result) { // #2
+                        result = {};
+                        result.success = false;
+                        result.msg = conn.responseText;
+                    }
+                    if (result.success) { // #3
+                        login.close(); // #4
+                        Ext.create('Packt.view.MyViewport'); // #5
+                    }
+                    else {
+                        Ext.Msg.show({
+                            title: 'Fail!',
+                            msg: result.msg, // #6
+                            icon: Ext.Msg.ERROR,
+                            buttons: Ext.Msg.OK
+                        });
+                    }
                 }
+
             })
         }
     }
